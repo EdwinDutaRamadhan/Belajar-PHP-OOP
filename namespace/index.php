8ad27693953a61fb33b33c1119c569975f3d12cb
EdwinDutaRamadhan <89692738+EdwinDutaRamadhan@users.noreply.github.com>
2022-09-23 01:25:02 +0700

    namespace

--- namespace/index.php
+++ namespace/index.php
@@ -1,9 +1,13 @@
 <?php
 require_once 'App/init.php';
-$produk1 = new Komik("Naruto", "masasi Kishimoto", "Shonen Jump", 30000, 100);
-$produk2 = new Game("Uncharted", "Neil Druckman", "Sony Computer", 250000, 50);
+// $produk1 = new Komik("Naruto", "masasi Kishimoto", "Shonen Jump", 30000, 100);
+// $produk2 = new Game("Uncharted", "Neil Druckman", "Sony Computer", 250000, 50);
 
-$cetakProduk = new CetakInfoProduk();
-$cetakProduk->tambahProduk($produk1);
-$cetakProduk->tambahProduk($produk2);
-echo $cetakProduk->cetak();
+// $cetakProduk = new CetakInfoProduk();
+// $cetakProduk->tambahProduk($produk1);
+// $cetakProduk->tambahProduk($produk2);
+// echo $cetakProduk->cetak();
+use App\Service\User as UserService;
+use App\Produk\User as UserProduk;
+new UserService();
+echo "<br>";
